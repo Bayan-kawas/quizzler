6bba0a48ac637e6170d4a262a83dc1dfc599c60b
unknown <bayan.kawas99@gmail.com>
2019-09-05 19:24:31 +0200

    encapsulation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+
+
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,20 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  QuizPages newquiz = QuizPages();
+
+  /* List<String> qustions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [false, true, true];*/
+// Questions ob1= Questions(question:'You can lead a cow down stairs but not up stairs.',answer:false);
+
+
+  List<Icon> degree = [];
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +55,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                newquiz.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +80,22 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+
+                setState(() {
+                  bool correctAnswer = newquiz.getQuestionAnswer();
+                  if (correctAnswer == true) {
+                    degree.add(Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ));
+                  } else {
+                    degree.add(Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ));
+                  }
+                  newquiz.goToNextQuestion();
+                });
               },
             ),
           ),
@@ -79,12 +113,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  bool correctAnswer = newquiz.getQuestionAnswer();
+                  if (correctAnswer == false) {
+                    degree.add(Icon(Icons.check, color: Colors.green));
+                  } else {
+                    degree.add(Icon(Icons.close, color: Colors.red));
+                  }
+                  newquiz.goToNextQuestion();
+                });
                 //The user picked false.
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: degree,
+        ),
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  String questionText;
+  bool   questionAnswer;
+  Question(String question , bool answer){
+   this.questionText=question;
+   this.questionAnswer=answer;
+  }
+}
